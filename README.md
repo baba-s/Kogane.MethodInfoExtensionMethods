1696a26a4fed4c93092d097d26422eec87891798
baba-s <shotababa1206@gmail.com>
2022-09-23 12:59:29 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Kogane.MethodInfoExtensionMethods
+# Kogane Method Info Extension Methods
+
+MethodInfo の拡張メソッド
+
+## 使用例
+
+```csharp
+Debug.Log( methodInfo.IsExtensionMethod() );
+```
---
+++ Runtime/MethodInfoExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace Kogane
+{
+    /// <summary>
+    /// MethodInfo の拡張メソッドを管理するクラス
+    /// </summary>
+    public static class MethodInfoExtensionMethods
+    {
+        /// <summary>
+        /// 拡張メソッドの場合 true を返します
+        /// </summary>
+        public static bool IsExtensionMethod( this MethodInfo self )
+        {
+            return self.IsDefined( typeof( ExtensionAttribute ), true );
+        }
+    }
+}
